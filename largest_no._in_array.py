423abebd1dd5394dc255fbf402f4f0438f393ee4
Akshat-124 <ad2323511@gmail.com>
2025-12-28 08:55:49 +0530

    DAY 5: Todays topics are merge sort, quick sort, largest number in array

---
+++ largest_no._in_array.py
@@ -0,0 +1,10 @@
+def largest_element(nums):
+    largest=nums[0]
+    n=len(nums)
+    for i in range (0,n):
+        largest=max(largest,nums[i])
+    return largest
+nums=[55,90,-99,3,67]
+print(largest_element(nums))
+
+
