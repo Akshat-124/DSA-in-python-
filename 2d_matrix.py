32f36bdffde89916443f052fea9164f2e324019c
Akshat-124 <ad2323511@gmail.com>
2026-01-03 22:17:17 +0530

    DAY 11: todays topics are longest consecuative sequence , two-D matrix

---
+++ 2d_matrix.py
@@ -0,0 +1,51 @@
+# def two_d_matix(nums):        #print 2d matrix
+#     rows=len(nums)
+#     cols=len(nums[0])
+#     for i in range (0,rows):
+#         for j in range (0,cols):
+#             print(nums[i][j])
+#         print()
+# nums=[[5,20,3],[7,-10,9],[1,-52,6]]
+# print(two_d_matix(nums))
+
+print("----------------------")
+
+# def upr_tri(nums):                      #upr triangle
+#     rows=len(nums)
+#     cols=len(nums[0])
+#     for i in range (0,rows):
+#         for j in range (0,cols):
+#             if j>=i:
+#                 print(nums[i][j])
+#             else:
+#                 print()
+# nums=[[5,20,3],[7,-10,9],[1,-52,6]]
+# print(upr_tri(nums))
+
+print("----------------------")
+
+# def lwr_tri(nums):                       #lwr triangle
+#     rows=len(nums)
+#     cols=len(nums[0])
+#     for i in range (0,rows):
+#         for j in range (0,cols):
+#             if i>=j:
+#                 print(nums[i][j])
+#             else:
+#                 print()
+# nums=[[5,20,3],[7,-10,9],[1,-52,6]]
+# print(lwr_tri(nums))
+
+print("----------------------")
+
+def lwr_tri(nums):                       #diagona'
+    rows=len(nums)
+    cols=len(nums[0])
+    for i in range (0,rows):
+        for j in range (0,cols):
+            if i==j:
+                print(nums[i][j])
+            else:
+                print()
+nums=[[5,20,3],[7,-10,9],[1,-52,6]]
+print(lwr_tri(nums))
---
+++ longest_consecutive_seq.py
@@ -0,0 +1,50 @@
+# def longest_seq(nums):                   #brute force (TC=o(n^2), SC=o(1))
+#     n=len(nums)
+#     max_count=0
+#     for i in range (0,n):
+#         num=nums[i]
+#         count=1
+#         while num+1 in nums:
+#             count+=1
+#             num = num+1
+#         max_count=max(max_count,count)
+#     return max_count
+# nums=[1,99,101,98,2,3,5,100]
+# print(longest_seq(nums))
+
+# def longest_seq(nums):                    #better sol (TC=o(n+nlogn))  
+#     n=len(nums)
+#     nums.sort()
+#     count=0
+#     last_smaller=float("-inf")
+#     longest=0
+#     for i in range(0,n):
+#         num=nums[i]
+#         if num-1 == last_smaller:
+#             count+=1
+#             last_smaller=num
+#         elif num != last_smaller:
+#             count = 1
+#             last_smaller=num
+#         longest=max(longest,count)
+#     return longest
+# nums=[1,99,101,98,2,3,5,100]
+# print(longest_seq(nums))
+
+def longest_seq(nums):
+    n=len(nums)
+    my_set=set()
+    for i in range(0,n):
+        my_set.add(nums[i])
+        longest=0
+    for num in my_set:
+        if num-1 not in my_set:
+            x=num
+            count=1
+            while x+1 in my_set:
+                count+=1
+                x+=1
+            longest=max(longest,count)
+    return longest
+nums=[1,99,101,98,2,3,5,100]
+print(longest_seq(nums))
