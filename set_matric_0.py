e82fbd0a788d7b4c4780bbfda3080247e736de9d
Akshat-124 <ad2323511@gmail.com>
2026-01-04 21:05:18 +0530

    DAY 12: Todays topics are rotating matrix by 90 degree , set matrix to 0

---
+++ set_matric_0.py
@@ -0,0 +1,42 @@
+# def mark_inf(matrix,row,col):                #bruteforce (TC=o(n*m)+o(n+m)+o(n*m))
+#     r=len(matrix)
+#     c=len(matrix[0])
+#     for i in range(0,r):
+#         if matrix[i][col]!=0:
+#             matrix[i][col]=float("inf")
+#     for j in range(0,c):
+#         if matrix [row][j]!=0:
+#             matrix [row][j]=float("inf")
+# def set_zeros(self,matrix):
+#     r=len(matrix)
+#     c=len(matrix[0])
+#     for i in range(0,r):
+#         for j in range(0,c):
+#             if matrix [i][j]==0:
+#                 self.mark_inf(matrix,i,j)
+#     for i in range (0,r):
+#         for j in range(0,c):
+#             if matrix [i][j]==float("inf"):
+#                 matrix[i][j]=0
+    
+# matrix=[[7,9,2,3],[20,8,0,10],[29,0,-10,5],[4,14,6,7]]
+# mark_inf(matrix,1,2)
+# print(matrix)
+
+def mark_zero(matrix):
+    r=len(matrix)
+    c=len(matrix[0])
+    rowtrack=[0 for _ in range (r)]
+    coltrack=[0 for _ in range (c)]
+    for i in range(0,r):
+        for j in range(0,c):
+            if matrix [i][j]==0:
+                rowtrack[i]=-1
+                coltrack[j]=-1
+    for i in range(0,r):
+        for j in range(0,c):
+            if rowtrack[i]==-1 or coltrack[j]==-1:
+                matrix[i][j]=0
+matrix=[[7,9,2,3],[20,8,0,10],[29,0,-10,5],[4,14,6,7]]
+mark_zero(matrix)
+print(matrix)
