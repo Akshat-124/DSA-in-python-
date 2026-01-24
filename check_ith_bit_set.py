26ef7d6d6cdb611bbbe16cb7c48ea30d2d3df188
Akshat <ad2323511@gmail.com>
2026-01-25 00:10:39 +0530

    Removed duplicate bit manipulation files from root

--- check_ith_bit_set.py
+++
@@ -1,7 +0,0 @@
-N = 13
-i = 1
-
-if (N & (1 << i)) != 0:  #left shift one time then & operator
-    print("SET")
-else:
-    print("NOT SET")
--- swap_two_no.py
+++
@@ -1,9 +0,0 @@
-a = 5
-b = 10
-print("Before:")
-print("a =", a, "b =", b)
-a = a ^ b    #xor
-b = a ^ b
-a = a ^ b
-print("After:")
-print("a =", a, "b =", b)
