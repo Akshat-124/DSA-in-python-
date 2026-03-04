1a9404e0ab6cac25b9c900a224a8c0ebac57a263
Akshat <ad2323511@gmail.com>
2026-03-04 22:56:05 +0530

    jump game

---
+++ arrays/jump_game_3.py
@@ -0,0 +1,11 @@
+def jump(nums):
+    jumps = 0
+    end = 0
+    farthest = 0
+    for i in range(len(nums) - 1):
+        farthest = max(farthest, i + nums[i])
+        if i == end:
+            jumps += 1
+            end = farthest
+    return jumps
+nums = [2,3,1,1,4]
