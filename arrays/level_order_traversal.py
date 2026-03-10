79ac1774635146887191bb062de1ff2872db4d3d
Akshat <ad2323511@gmail.com>
2026-03-10 22:27:25 +0530

    traversal

---
+++ arrays/level_order_traversal.py
@@ -0,0 +1,26 @@
+from collections import deque
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.left = None
+        self.right = None
+def level_order(root):
+    if root is None:
+        return
+    q = deque()
+    q.append(root)
+    while q:
+        node = q.popleft()
+        print(node.data, end=" ")
+        if node.left:
+            q.append(node.left)
+        if node.right:
+            q.append(node.right)
+root = Node(1)
+root.left = Node(2)
+root.right = Node(3)
+root.left.left = Node(4)
+root.left.right = Node(5)
+root.right.left = Node(6)
+root.right.right = Node(7)
+level_order(root)
