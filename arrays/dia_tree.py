ef95370af2ed0729c45fc4b0d18f4f2aad16b1ef
Akshat <ad2323511@gmail.com>
2026-03-12 23:13:29 +0530

    diameter of tree

---
+++ arrays/dia_tree.py
@@ -0,0 +1,24 @@
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+class Solution:
+    def diameterOfBinaryTree(self, root):
+        self.diameter = 0
+        def dfs(node):
+            if not node:
+                return 0
+            left = dfs(node.left)
+            right = dfs(node.right)
+            self.diameter = max(self.diameter, left + right)
+            return 1 + max(left, right)
+        dfs(root)
+        return self.diameter
+root = TreeNode(1)
+root.left = TreeNode(2)
+root.right = TreeNode(3)
+root.left.left = TreeNode(4)
+root.left.right = TreeNode(5)
+sol = Solution()
+print(sol.diameterOfBinaryTree(root))
