52a4c7d2c0ce87c735520ce0a7e374b5b908fa90
Akshat <ad2323511@gmail.com>
2026-07-26 16:13:50 +0530

    day 19

---
+++ dsa revision/leetcode_13_Roman_to_integer.py
@@ -0,0 +1,12 @@
+class Solution:
+    def romanToInt(self, s: str) -> int:
+        res = 0
+        letters = {"I":1,"V":5,"X":10,"L":50,"C":100,"D":500,"M":1000}
+        n=len(s)
+        for i in range (0,n):
+            if i<n-1 and letters[s[i]]<letters[s[i+1]]:
+                res -= letters[s[i]]
+            else:
+                res += letters[s[i]]
+        return res
+        
