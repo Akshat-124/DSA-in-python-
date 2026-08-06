41c65a928db362fef80d2bc8008a3c70120a8cee
Akshat <ad2323511@gmail.com>
2026-08-06 23:33:53 +0530

    day 29

---
+++ dsa revision/leetcode_3345. Smallest Divisible Digit Product I.py
@@ -0,0 +1,14 @@
+class Solution:
+    def smallestNumber(self, n: int, t: int) -> int:
+        def prod(x):
+            p = 1
+            while x:
+                p *= x % 10
+                x //= 10
+            return p
+        
+        while True:
+            if prod(n) % t == 0:
+                return n
+            n += 1
+        
